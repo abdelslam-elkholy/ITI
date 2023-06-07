6b2a0feaecace618a48c9b5b5da61812526c6137
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-07 19:04:16 +0300

    first-day-tasks

--- Mearn-Track/JavaScript/Day-1/script.js
+++ Mearn-Track/JavaScript/Day-1/script.js
@@ -4,41 +4,60 @@
 1- What is the output of this piece of code
 var y;
 console.log(y);
-*/
-
 // Will log Undefined
 
-/*
+
 2- What is the output of this piece of code
 console.log(y);
-*/
+// Reference Error
 
-/*
+
 3- What is the output of this piece of code
 var x=10;
 var y = 20;
 console.log(y-x*2);
+// will output zero
+
+
 4- What is the output of this piece of code
 var y;
 console.log(typeof y);
+// will output undefined
+
+
 5- What is the output of this piece of code
 var x ="1";
 var y = 2;
 console.log(x+y);
+// will output "12"
+
+
 6- What is the output of this piece of code
 var x = 1;
 var y = true;
-console.log(x+y);7- What is the output of this piece of code
+console.log(x+y);
+// will output 2
+
+
+7- What is the output of this piece of code
 var x = 12;
 var y = "6";
 console.log(x/y);
+//will output 2
+
+
 8-What is the output of this piece of code
 console.log(15 + 3 + "number");
 console.log("number" + 15 + 3);
+//18number
+//number153
+
+
 9- What is the output of this piece of code
 var x = false;
 var y = !!x;
 console.log(y);
+//
 10-What is the output of this piece of code
 var x = false;
 console.log(typeof x ==0)
