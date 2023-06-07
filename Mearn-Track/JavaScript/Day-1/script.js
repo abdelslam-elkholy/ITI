6b0d8abf306825141686ef4eaa36b8f55e1c04b3
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-07 18:48:57 +0300

    first-day-tasks

--- Mearn-Track/JavaScript/Day-1/script.js
+++ Mearn-Track/JavaScript/Day-1/script.js
@@ -1,1 +1,51 @@
 "use strict";
+/*
+
+1- What is the output of this piece of code
+var y;
+console.log(y);
+*/
+
+// Will log Undefined
+
+/*
+2- What is the output of this piece of code
+console.log(y);
+*/
+
+/*
+3- What is the output of this piece of code
+var x=10;
+var y = 20;
+console.log(y-x*2);
+4- What is the output of this piece of code
+var y;
+console.log(typeof y);
+5- What is the output of this piece of code
+var x ="1";
+var y = 2;
+console.log(x+y);
+6- What is the output of this piece of code
+var x = 1;
+var y = true;
+console.log(x+y);7- What is the output of this piece of code
+var x = 12;
+var y = "6";
+console.log(x/y);
+8-What is the output of this piece of code
+console.log(15 + 3 + "number");
+console.log("number" + 15 + 3);
+9- What is the output of this piece of code
+var x = false;
+var y = !!x;
+console.log(y);
+10-What is the output of this piece of code
+var x = false;
+console.log(typeof x ==0)
+11- Write a JavaScript code to display in console the odd numbers from 1 to 9
+(use loop)
+12-On your page, show alert for the user that say “Welcome to my site”, then
+show him prompt ask him to enter his name and write to the page “welcome +
+his name”.
+
+*/
