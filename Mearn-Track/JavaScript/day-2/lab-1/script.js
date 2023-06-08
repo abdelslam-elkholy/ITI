e8329de885a76752f32fc252838699b46547c263
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-08 15:21:30 +0300

    Day-2 confirmed

--- Mearn-Track/JavaScript/day-2/lab-1/script.js
+++ Mearn-Track/JavaScript/day-2/lab-1/script.js
@@ -14,19 +14,22 @@ for (let i = 0; i < word.length; i++) {
 
 document.write(`Numbers of e is ${count}`);
 
-let caseSen = prompt("for case sensetive ok or any char");
+let caseSen = prompt("for case sensetive enter 1");
 
 function isPalindrome(str) {
-  if (caseSen) {
-    const origStr = str.toLowerCase();
-    const reversedStr = origStr.split("").reverse().join("");
-    origStr === reversedStr
-      ? console.log("The string is a palindrome.")
-      : console.log("The string is not a palindrome.");
-  } else {
+  if (caseSen == 1) {
     const reversedStr = str.split("").reverse().join("");
     str === reversedStr
       ? console.log("The string is not a palindrome.")
       : console.log("The string is not a palindrome.");
+  } else {
+    const origStr = str.toLowerCase();
+    const reversedStr = origStr.split("").reverse().join("");
+
+    origStr === reversedStr
+      ? console.log("The string is a palindrome.")
+      : console.log("The string is not a palindrome.");
   }
 }
+
+isPalindrome(word);
---
+++ Mearn-Track/JavaScript/day-2/lab-2/script.js
@@ -0,0 +1,65 @@
+"use strict";
+// let userName;
+// let phoneNum;
+// let mobileNum;
+// const mobileNumRegex = /^(010|011|012)\d{8}$/;
+// do {
+//   userName = prompt("enter your name: ");
+// } while (!userName || !isNaN(userName));
+
+// do {
+//   phoneNum = prompt("enter phone number: ");
+// } while (!phoneNum || phoneNum.length < 8 || isNaN(phoneNum));
+
+// do {
+//   mobileNum = prompt("enter mobile number: ");
+// } while (!phoneNum || !mobileNumRegex.test(mobileNum));
+
+const fillingArr = (num) => {
+  // let arr;
+  let arr = [];
+  let el;
+  for (let i = 0; i < num; i++) {
+    el = prompt(`Enter the element number ${i + 1}`);
+    arr.push(el);
+  }
+
+  document.write(
+    `<p style="color: red; font-size: 1.5rem"> sum of 3 values <span style="black: red; font-size: 1rem">${
+      arr[0]
+    } + ${arr[1]} + ${arr[2]} = ${arr[0] + arr[1] + arr[2]} </span></p>`
+  );
+  document.write(
+    `<p style="color: red; font-size: 1.5rem"> multipication of 3 values <span style="black: red; font-size: 1rem">${
+      arr[0]
+    } * ${arr[1]} * ${arr[2]} = ${arr[0] * arr[1] * arr[2]} </span></p>`
+  );
+
+  document.write(
+    `<p style="color: red; font-size: 1.5rem"> dividtion of 3 values <span style="black: red; font-size: 1rem">${
+      arr[0]
+    } / ${arr[1]} / ${arr[2]} = ${arr[0] / arr[1] / arr[2]} </span></p>`
+  );
+
+  console.log(arr.sort((a, b) => a - b));
+  console.log(arr.sort((a, b) => b - a));
+};
+
+// fillingArr(5);
+// let arr = [];
+// let el;
+// for (let i = 0; i < 3; i++) {
+//   el = prompt(`Enter the element number ${i + 1}`);
+//   arr.push(el);
+// }
+
+/////////////////////////////////////////
+
+let radius = prompt("Enter circle radius");
+alert(`The area = ${Math.PI * radius ** 2}`);
+
+let sqrt = Number(prompt("Enter number"));
+alert(`the sqrt of ${sqrt} is ${Math.sqrt(sqrt)}`);
+
+// let angle = Number(prompt("Enter number"));
+// alert(`the cos of ${angle} is ${Math.cos(angle)}`);
