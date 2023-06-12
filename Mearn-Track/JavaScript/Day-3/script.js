8fcd6079a8d8e5b234e4239a979506257287b403
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-12 12:33:03 +0300

    day-3 tasks

--- Mearn-Track/JavaScript/Day-3/script.js
+++ Mearn-Track/JavaScript/Day-3/script.js
@@ -1,1 +1,16 @@
 "use script";
+
+const displayTime = () => alert(`Time IS: ${new Date().toLocaleTimeString()}`);
+
+let win;
+
+const displayWindow = () => {
+  setTimeout(() => {
+    win = window.open("", "MsgWindow", "width=200,height=100");
+    win.document.write(
+      "<p>This is 'MsgWindow'. I am 200px wide and 100px tall!</p>"
+    );
+  }, 3000);
+};
+
+const closeWindow = () => win.close();
