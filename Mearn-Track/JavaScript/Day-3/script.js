dcbe4a2918d737976cbe7264d03278c25de8bfc9
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-12 12:50:54 +0300

    day-3 tasks

--- Mearn-Track/JavaScript/Day-3/script.js
+++ Mearn-Track/JavaScript/Day-3/script.js
@@ -14,3 +14,42 @@ const displayWindow = () => {
 };
 
 const closeWindow = () => win.close();
+
+////////////////////////////////////////////////////////////////
+
+let userName;
+let phoneNum;
+let mobileNum;
+let mail;
+let color;
+let colorCheck;
+const userNameRegex = /[a-z A-Z]{3,}/;
+const phoneNumRegex = /[0-9]{8}/;
+const mobileNumRegex = /^(010|011|012)[0-9]{8}$/;
+const mailRegex = /^[a-zA-Z]{3,9}(@123.com)$/;
+
+do {
+  userName = prompt("enter your name: ");
+} while (!userNameRegex.test(userName));
+
+do {
+  phoneNum = prompt("enter phone number: ");
+} while (!phoneNum.test(phoneNum));
+
+do {
+  mobileNum = prompt("enter mobile number: ");
+} while (!mobileNum.test(mobileNum));
+
+do {
+  mail = prompt("enter Email: ");
+} while (!mailRegex.test(mail));
+
+do {
+  color = prompt("enter Color: ");
+
+  colorCheck = color.toLocaleLowerCase();
+} while (
+  colorCheck !== "red" ||
+  colorCheck !== "green" ||
+  colorCheck !== "blue"
+);
