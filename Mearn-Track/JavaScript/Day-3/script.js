3fe1780e7829b894fab7073e4f340e27a10bb65b
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-13 12:09:25 +0300

    day-4 tasks

--- Mearn-Track/JavaScript/Day-3/script.js
+++ Mearn-Track/JavaScript/Day-3/script.js
@@ -1,22 +1,21 @@
 "use script";
 
-// const displayTime = () => alert(`Time IS: ${new Date().toLocaleTimeString()}`);
+const displayTime = () => alert(`Time IS: ${new Date().toLocaleTimeString()}`);
 
-// let win;
+let win;
 
-// const displayWindow = () => {
-//   setTimeout(() => {
-//     win = window.open("", "MsgWindow", "width=200,height=100");
-//     win.document.write(
-//       "<p>This is 'MsgWindow'. I am 200px wide and 100px tall!</p>"
-//     );
-//   }, 3000);
-// };
+const displayWindow = () => {
+  setTimeout(() => {
+    win = window.open("", "MsgWindow", "width=200,height=100");
+    win.document.write(
+      "<p>This is 'MsgWindow'. I am 200px wide and 100px tall!</p>"
+    );
+  }, 3000);
+};
 
-// const closeWindow = () => win.close();
+const closeWindow = () => win.close();
 
 /*
-
 even
 \b[02468]+\b
 
