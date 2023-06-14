5d75c96f00710d713978ddc37483933f75cbd3f9
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-14 15:14:08 +0300

    Day-4 tasks Confirmed

--- Mearn-Track/JavaScript/Day-4/task-2/script.js
+++ Mearn-Track/JavaScript/Day-4/task-2/script.js
@@ -6,20 +6,21 @@ console.log(messageBtn);
 const showMessage = () => {
   const text = `Hey im your text in the new window`;
   const windowMessage = window.open("", "_blank", "width=500,height=300");
-  // const newDiv = document.createElement("div");
   let count = 0;
 
+  // const newDiv = document.createElement("div");
   const typeMessage = setInterval(() => {
     // newDiv.textContent += text[count];
     // windowMessage.document.body.appendChild(newDiv);
+
     windowMessage.document.write(`${text[count]}`);
     count++;
 
     if (count >= text.length) {
       clearInterval(typeMessage);
-      windowMessage.close();
+      setTimeout(windowMessage.close(), 1000);
     }
-  }, 300);
+  }, 200);
 };
 
 messageBtn.addEventListener("click", showMessage);
