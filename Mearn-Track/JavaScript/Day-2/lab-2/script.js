cafd43242e532bcdc8dfef2ebb590f05b4322ffe
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-14 15:07:56 +0300

    Day-2 tasks Confirmed

--- Mearn-Track/JavaScript/Day-2/lab-2/script.js
+++ Mearn-Track/JavaScript/Day-2/lab-2/script.js
@@ -19,33 +19,35 @@ const fillingArr = (num) => {
   // let arr;
   let arr = [];
   let el;
+
   for (let i = 0; i < num; i++) {
     el = prompt(`Enter the element number ${i + 1}`);
     arr.push(el);
   }
-
+  let sum = 0;
+  let multi = 1;
+  let div = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    sum += +arr[i];
+    multi *= arr[i];
+    div /= arr[i];
+  }
+  div *= arr[0];
   document.write(
-    `<p style="color: red; font-size: 1.5rem"> sum of 3 values <span style="black: red; font-size: 1rem">${
-      arr[0]
-    } + ${arr[1]} + ${arr[2]} = ${arr[0] + arr[1] + arr[2]} </span></p>`
-  );
-  document.write(
-    `<p style="color: red; font-size: 1.5rem"> multipication of 3 values <span style="black: red; font-size: 1rem">${
-      arr[0]
-    } * ${arr[1]} * ${arr[2]} = ${arr[0] * arr[1] * arr[2]} </span></p>`
-  );
-
-  document.write(
-    `<p style="color: red; font-size: 1.5rem"> dividtion of 3 values <span style="black: red; font-size: 1rem">${
-      arr[0]
-    } / ${arr[1]} / ${arr[2]} = ${arr[0] / arr[1] / arr[2]} </span></p>`
+    `<p style="color: red; font-size: 1.5rem"> sum of 3 values <span style="black: red; font-size: 1rem">${arr.toString(
+      "+"
+    )} </span> is ${sum}</p> <p style="color: red; font-size: 1.5rem"> multiplication of 3 values <span style="black: red; font-size: 1rem">${arr.toString(
+      "*"
+    )} </span> is ${multi}</p><p style="color: red; font-size: 1.5rem"> divistion of 3 values <span style="black: red; font-size: 1rem">${arr.toString(
+      "/"
+    )} </span> is ${div}</p>`
   );
 
   console.log(arr.sort((a, b) => a - b));
   console.log(arr.sort((a, b) => b - a));
 };
 
-// fillingArr(5);
+fillingArr(3);
 // let arr = [];
 // let el;
 // for (let i = 0; i < 3; i++) {
@@ -55,11 +57,11 @@ const fillingArr = (num) => {
 
 /////////////////////////////////////////
 
-let radius = prompt("Enter circle radius");
-alert(`The area = ${Math.PI * radius ** 2}`);
+// let radius = prompt("Enter circle radius");
+// alert(`The area = ${Math.PI * radius ** 2}`);
 
-let sqrt = Number(prompt("Enter number"));
-alert(`the sqrt of ${sqrt} is ${Math.sqrt(sqrt)}`);
+// let sqrt = Number(prompt("Enter number"));
+// alert(`the sqrt of ${sqrt} is ${Math.sqrt(sqrt)}`);
 
 // let angle = Number(prompt("Enter number"));
 // alert(`the cos of ${angle} is ${Math.cos(angle)}`);
