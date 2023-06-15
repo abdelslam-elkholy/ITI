67d8d6acc8342e6ef5102da4b637c9a222b04bf5
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-15 14:07:30 +0300

    day-6

--- Mearn-Track/JavaScript/Day-6/form.js
+++ Mearn-Track/JavaScript/Day-6/form.js
@@ -1,9 +1,26 @@
 "use strict";
 
-const name = document.querySelector("#name");
-const age = document.querySelector("#age");
-const mail = document.querySelector("#email");
+const form = document.querySelector("#form");
 const tableBody = document.querySelector("#tbody");
-
 const addBtn = document.querySelector("#addBtn");
 const resetBtn = document.querySelector("#resetBtn");
+
+addBtn.addEventListener("click", () => {
+  const fName = document.querySelector("#name").value;
+  const age = document.querySelector("#age").value;
+  const mail = document.querySelector("#email").value;
+
+  if (fName && age && mail) {
+    const row = `
+    <tr>        
+    <td>${fName}</td>
+    <td>${age}</td>
+    <td>${mail}</td>
+    </tr>
+    `;
+    tableBody.appendChild(row);
+    form.reset();
+  } else {
+    alert("Fill Data");
+  }
+});
