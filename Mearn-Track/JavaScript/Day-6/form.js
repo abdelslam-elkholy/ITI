075dfb34ceb83feec954a1fcd47e590213feacbc
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-15 13:58:49 +0300

    day-6

--- Mearn-Track/JavaScript/Day-6/form.js
+++ Mearn-Track/JavaScript/Day-6/form.js
@@ -3,3 +3,7 @@
 const name = document.querySelector("#name");
 const age = document.querySelector("#age");
 const mail = document.querySelector("#email");
+const tableBody = document.querySelector("#tbody");
+
+const addBtn = document.querySelector("#addBtn");
+const resetBtn = document.querySelector("#resetBtn");
