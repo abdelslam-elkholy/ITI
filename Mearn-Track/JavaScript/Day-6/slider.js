9fe4aa74393bccc1d7629d91bd79816c3fa1e18f
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-15 12:39:35 +0300

    day-6

---
+++ Mearn-Track/JavaScript/Day-6/slider.js
@@ -0,0 +1,27 @@
+"use strict ";
+
+const startBtn = document.querySelector("#start");
+const stopBtn = document.querySelector("#stop");
+const nextBtn = document.querySelector("#next");
+const prevBtn = document.querySelector("#prev");
+const imgTag = document.querySelector("#img");
+const images = [
+  "./1.jpg",
+  "./2.jpg",
+  "./3.jpg",
+  "./4.jpg",
+  "./5.jpg",
+  "./6.jpg",
+];
+
+let currentIndex = 0;
+let img;
+
+const setImg = () => {
+  img = setInterval(() => imgTag.attributes("src", images[currentIndex]), 1000);
+};
+
+const startShow = () => {
+  currentIndex === images.length - 1 ? (currentIndex = 0) : currentIndex++;
+  setImg();
+};
