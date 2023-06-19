83f6a03aa987fdacf3b457f5a37b568ca87e261f
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-19 13:31:04 +0300

    day-8-tasks

--- Mearn-Track/JavaScript/Day-8/task1.js
+++ Mearn-Track/JavaScript/Day-8/task1.js
@@ -29,64 +29,15 @@ const setCookie = function (key, val, date) {
 };
 
 const getCookie = (name) => {
-  let coockie = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith(name))
-    ?.split("=")[1];
+  let coockie = hasCoocie(name)?.split("=")[1];
+  return coockie;
 };
 
 const deleteCookie = (name) => {
   document.cookie = `${name} =; expires = expires=Thu, 01 Jan 1970 00:00:00 UTC`;
 };
 
-var CookieLibrary = {
-  getCookie: function (cookieName) {
-    var name = cookieName + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var cookieArray = decodedCookie.split(";");
-    for (var i = 0; i < cookieArray.length; i++) {
-      var cookie = cookieArray[i];
-      while (cookie.charAt(0) === " ") {
-        cookie = cookie.substring(1);
-      }
-      if (cookie.indexOf(name) === 0) {
-        return cookie.substring(name.length, cookie.length);
-      }
-    }
-    return "";
-  },
-
-  setCookie: function (cookieName, cookieValue, expiryDate) {
-    var expires = "";
-    if (expiryDate) {
-      var date = new Date();
-      date.setTime(date.getTime() + expiryDate * 24 * 60 * 60 * 1000);
-      expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = cookieName + "=" + cookieValue + expires + "; path=/";
-  },
-
-  deleteCookie: function (cookieName) {
-    document.cookie =
-      cookieName + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-  },
-
-  allCookieList: function () {
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var cookieArray = decodedCookie.split(";");
-    var cookies = [];
-    for (var i = 0; i < cookieArray.length; i++) {
-      var cookie = cookieArray[i];
-      while (cookie.charAt(0) === " ") {
-        cookie = cookie.substring(1);
-      }
-      cookies.push(cookie);
-    }
-    return cookies;
-  },
-
-  hasCookie: function (cookieName) {
-    var cookieValue = this.getCookie(cookieName);
-    return cookieValue !== "";
-  },
+const hasCoocie = (name) => {
+  let coockie = document.cookie.split("; ").find((row) => row.startsWith(name));
+  return coockie !== "";
 };
