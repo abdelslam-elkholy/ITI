5225d3b7b4c77633bef80d7ce91e73272366cac5
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-21 20:37:11 +0300

    day-10-tasks

--- Mearn-Track/JavaScript/Day-10/script.js
+++ Mearn-Track/JavaScript/Day-10/script.js
@@ -48,4 +48,22 @@ console.log(checkDublicates([1, 2, 4, 5, 6]));
 //     "FizzBuzz"
 // ]
 
-const checkFizzBuzz = (n) => {};
+const checkFizzBuzz = (n) => {
+  const arr = [];
+  let i = 1;
+  while (i <= n) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      arr.push("FizzBuzz");
+    } else if (i % 3 == 0) {
+      arr.push("Fizz");
+    } else if (i % 5 == 0) {
+      arr.push("Buzz");
+    } else {
+      arr.push(i);
+    }
+    i++;
+  }
+  console.log(arr);
+};
+
+checkFizzBuzz(15);
