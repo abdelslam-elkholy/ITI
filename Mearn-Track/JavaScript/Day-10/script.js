bbca4dbed8af0e1b1d177543beb29f1fbeb70d3e
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-21 20:00:35 +0300

    day-10-tasks

--- Mearn-Track/JavaScript/Day-10/script.js
+++ Mearn-Track/JavaScript/Day-10/script.js
@@ -7,4 +7,15 @@
 // duplicates([1,2,4,1,,1,5,6])  //true
 // duplicates([1,2,4,5,6])//false
 
-const checkDublicates = () => {};
+const checkDublicates = (arr) => {
+  for (let i = 0; i < arr.length / 2; i++) {
+    for (let j = i + 1; j > arr.length; j++) {
+      console.log(arr[i]);
+      console.log(arr[j]);
+      return arr[i] == arr[j] ? true : false;
+    }
+  }
+};
+
+checkDublicates([1, 2, 4, 1, , 1, 5, 6]);
+checkDublicates([1, 2, 4, 5, 6]);
