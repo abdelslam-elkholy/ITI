2b3df13b0586f2002c44ed81e73eafbeb88730ea
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-21 19:56:05 +0300

    day-10-tasks

--- Mearn-Track/JavaScript/Day-10/script.js
+++ Mearn-Track/JavaScript/Day-10/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+// Given an array of integers, find if the array contains any duplicates. Your
+// function should return true if any value appears at least twice in the array,
+// and it should return false if every element is distinct.
+
+// duplicates([1,2,4,1,,1,5,6])  //true
+// duplicates([1,2,4,5,6])//false
+
+const checkDublicates = () => {};
