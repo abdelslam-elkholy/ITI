e9b7ecdb5722aa5e2e9898e82b086e3a57e3c3a0
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-21 20:25:17 +0300

    day-10-tasks

--- Mearn-Track/JavaScript/Day-10/script.js
+++ Mearn-Track/JavaScript/Day-10/script.js
@@ -18,3 +18,34 @@ const checkDublicates = (arr) => {
 
 console.log(checkDublicates([1, 2, 4, 1, , 1, 5, 6]));
 console.log(checkDublicates([1, 2, 4, 5, 6]));
+
+// Write a program that outputs the string representation of numbers from 1 to n.
+
+// But for multiples of three it should output “Fizz” instead of the number and
+// for the multiples of five output “Buzz”. For numbers which are multiples of
+// both three and five output “FizzBuzz”.
+
+// Example:
+
+// n = 15,
+
+// Return:
+// [
+//     "1",
+//     "2",
+//     "Fizz",
+//     "4",
+//     "Buzz",
+//     "Fizz",
+//     "7",
+//     "8",
+//     "Fizz",
+//     "Buzz",
+//     "11",
+//     "Fizz",
+//     "13",
+//     "14",
+//     "FizzBuzz"
+// ]
+
+const checkFizzBuzz = (n) => {};
