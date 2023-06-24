cfe99e680e4d354bd1af7644dca5dc64d02e50c0
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-24 23:55:38 +0300

    Day-9

--- Mearn-Track/JavaScript/Day-9/script.js
+++ Mearn-Track/JavaScript/Day-9/script.js
@@ -1,10 +1,35 @@
 "use strict";
 
-function List(start, end, steps) {
+function List(st, en, ste) {
+  let start = st;
+  let end = en;
+  let steps = ste;
+
   this.length = 0;
   const list = {};
 
   function fillList() {
-    // for(let i =0; i< (end - start)/steps)
+    let i = 0;
+    let item = start;
+    // console.log(`loop number ${i + 1}`);
+    while (item <= end) {
+      //   console.log(`loop number ${i + 1}`);
+      list[i] = item;
+      this.length++;
+      item += steps;
+      i++;
+    }
   }
+
+  fillList.call(this);
+
+  this.getList = function () {
+    return list;
+  };
 }
+
+const newlist = new List(2, 10, 2);
+// newlist.fillList();
+// console.log(newlist);
+console.log(newlist.getList());
+console.log(`this is the list length ` + newlist.length);
