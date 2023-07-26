4dcb2f68439cc4a1f80b8125b5bb1459fd0e7a62
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-27 02:52:52 +0300

    update

--- Mearn-Track/MongoDb/Day-2/models/studentModel.js
+++ Mearn-Track/MongoDb/Day-2/models/studentModel.js
@@ -1,3 +1,18 @@
 const mongoose = require("mongoose");
 
-const studentSchema = new mongoose.Schema({});
+const courseSchema = new mongoose.Schema({
+  CourseID: { type: String, required: true },
+  grade: { type: String, required: true },
+});
+
+const studentSchema = new mongoose.Schema({
+  FirstName: { type: String, required: true },
+  LastName: { type: String, required: true },
+  IsFired: { type: Boolean, default: false },
+  FacultyID: { type: mongoose.Schema.Types.ObjectId, ref: "Faculty" },
+  courses: [courseSchema],
+});
+
+const Student = mongoose.model("Student", studentSchema);
+
+module.exports = Student;
