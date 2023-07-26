542520d3f64da147f4d3d02eb152ae0047b389e3
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-27 02:21:12 +0300

    update

--- Mearn-Track/MongoDb/Day-2/server.js
+++ Mearn-Track/MongoDb/Day-2/server.js
@@ -1,11 +1,26 @@
+const app = require("./app");
 const mongoose = require("mongoose");
-
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
-const express = require("express");
-const app = express();
 
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(DB);
+    console.log("connected To Db ");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+dbConnect();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`listening From Port ${port}`);
+});
