706e7cf623723c441c8f10dfba78737a4f934f6a
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-27 06:03:22 +0300

    update

--- Mearn-Track/MongoDb/Day-2/client/app.js
+++ Mearn-Track/MongoDb/Day-2/client/app.js
@@ -1,113 +1,115 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function () {
-  function fetchFaculties() {
-    fetch("http://localhost:3000/api/faculty")
-      .then((response) => response.json())
-      .then((faculties) => {
-        console.log(faculties);
+const fetchFaculties = async () => {
+  try {
+    let facultySelect = document.getElementById("facultySelect");
+    const data = await fetch("http://localhost:3000/api/faculty");
+    const res = await data.json();
+    const faculties = res.data.faculties;
 
-        let facultySelect = document.getElementById("facultySelect");
-        facultySelect.innerHTML = "";
+    console.log(faculties);
 
-        faculties.data.faculties.forEach((faculty) => {
-          let option = document.createElement("option");
-          option.value = faculty._id;
-          option.textContent = faculty.FacultyName;
-          facultySelect.appendChild(option);
-        });
-      })
-      .catch((error) => console.error("Error fetching faculties:", error));
-  }
+    facultySelect.innerHTML = "";
 
-  function fetchStudents() {
-    fetch("http://localhost:3000/api/students")
-      .then((response) => response.json())
-      .then((students) => {
-        console.log(students);
-        let studentList = "";
-        students.data.students.forEach((student) => {
-          studentList += `<li>${student.FirstName} ${student.LastName} (Faculty: ${Faculty.FacultyName})</li>`;
-        });
-        document.getElementById("studentList").innerHTML = studentList;
-      })
-      .catch((error) => console.error("Error fetching students:", error));
-  }
-
-  // Function to fetch and display aggregations
-  function fetchAggregations() {
-    fetch("http://localhost:3000/api/stats")
-      .then((response) => response.json())
-      .then((aggregations) => {
-        document.getElementById("sumFinalMarks").textContent =
-          aggregations.sumFinalMarks;
-        document.getElementById("studentCount").textContent =
-          aggregations.studentCount;
-
-        let studentsCountInFacultiesList = "";
-        for (const faculty of aggregations.studentsCountInFaculties) {
-          studentsCountInFacultiesList += `<li>${faculty._id}: ${faculty.count}</li>`;
-        }
-        document.getElementById("studentsCountInFaculties").innerHTML =
-          studentsCountInFacultiesList;
-
-        let studentsWithAvgGradeList = "";
-        for (const student of aggregations.studentsWithAvgGrade) {
-          studentsWithAvgGradeList += `<li>${student.FullName}: ${student.AvgGrade}</li>`;
-        }
-        document.getElementById("studentsWithAvgGrade").innerHTML =
-          studentsWithAvgGradeList;
-
-        let studentsWithFacultyList = "";
-        for (const student of aggregations.studentsWithFaculty) {
-          studentsWithFacultyList += `<li>${student.FullName}: FacultyID: ${student.Faculty.FacultyID}, Faculty Name: ${student.Faculty.FacultyName}</li>`;
-        }
-        document.getElementById("studentsWithFaculty").innerHTML =
-          studentsWithFacultyList;
-      })
-      .catch((error) => console.error("Error fetching aggregations:", error));
-  }
-
-  // Event listener for the "Add Student" button
-  document
-    .getElementById("submitStudentBtn")
-    .addEventListener("click", function () {
-      const firstName = document.getElementById("firstName").value;
-      const lastName = document.getElementById("lastName").value;
-      const facultyID = document.getElementById("facultySelect").value;
-
-      // Create a new student object
-      const newStudent = {
-        FirstName: firstName,
-        LastName: lastName,
-        FacultyID: facultyID,
-        courses: [],
-      };
-
-      // POST request to add the new student
-      fetch("http://localhost:3000/api/students", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newStudent),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("New student added:", data);
-          // Clear form fields
-          document.getElementById("firstName").value = "";
-          document.getElementById("lastName").value = "";
-
-          // Refresh student list and aggregations
-          fetchStudents();
-          fetchAggregations();
-        })
-        .catch((error) => console.error("Error adding student:", error));
+    faculties.forEach((faculty) => {
+      let option = document.createElement("option");
+      option.value = faculty._id;
+      option.textContent = faculty.FacultyName;
+      facultySelect.appendChild(option);
     });
+  } catch {
+    (error) => console.error("Error fetching faculties:", error);
+  }
+};
 
-  // Call the functions to fetch faculties, students, and aggregations
-  fetchFaculties();
-  fetchStudents();
-  fetchAggregations();
-});
+const fetchStudents = async () => {
+  try {
+    const data = await fetch("http://localhost:3000/api/students");
+    const res = await data.json();
+    const students = res.data.students;
+    console.log(students);
+    let studentList = "";
+    students.forEach((student) => {
+      studentList += `<li>${student.FirstName} ${student.LastName} (Faculty: ${student.FacultyName})</li>`;
+    });
+    document.getElementById("studentList").innerHTML = studentList;
+  } catch {
+    (error) => console.error("Error fetching students:", error);
+  }
+};
+
+const fetchAggregations = async () => {
+  try {
+    const data = await fetch("http://localhost:3000/api/stats");
+    const res = await data.json();
+    const aggregations = res.data.aggregations;
+    document.getElementById("sumFinalMarks").textContent =
+      aggregations.sumFinalMarks;
+    document.getElementById("studentCount").textContent =
+      aggregations.studentCount;
+
+    let studentsCountInFacultiesList = "";
+    for (const faculty of aggregations.studentsCountInFaculties) {
+      studentsCountInFacultiesList += `<li>${faculty._id}: ${faculty.count}</li>`;
+    }
+    document.getElementById("studentsCountInFaculties").innerHTML =
+      studentsCountInFacultiesList;
+
+    let studentsWithAvgGradeList = "";
+    for (const student of aggregations.studentsWithAvgGrade) {
+      studentsWithAvgGradeList += `<li>${student.FullName}: ${student.AvgGrade}</li>`;
+    }
+    document.getElementById("studentsWithAvgGrade").innerHTML =
+      studentsWithAvgGradeList;
+
+    let studentsWithFacultyList = "";
+    for (const student of aggregations.studentsWithFaculty) {
+      studentsWithFacultyList += `<li>${student.FullName}: FacultyID: ${student.Faculty.FacultyID}, Faculty Name: ${student.Faculty.FacultyName}</li>`;
+    }
+    document.getElementById("studentsWithFaculty").innerHTML =
+      studentsWithFacultyList;
+  } catch {
+    (error) =>
+      console.error("Error fetching aggregations.data.aggregations:", error);
+  }
+};
+
+const createStudent = () => {
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const facultyID = document.getElementById("facultySelect").value;
+
+  const newStudent = {
+    FirstName: firstName,
+    LastName: lastName,
+    FacultyID: facultyID,
+    courses: [],
+  };
+
+  fetch("http://localhost:3000/api/students", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newStudent),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("New student added:", data);
+
+      document.getElementById("firstName").value = "";
+      document.getElementById("lastName").value = "";
+
+      fetchStudents();
+      fetchAggregations();
+    })
+    .catch((error) => console.error("Error adding student:", error));
+};
+
+document
+  .getElementById("submitStudentBtn")
+  .addEventListener("click", createStudent);
+
+fetchFaculties();
+fetchStudents();
+fetchAggregations();
