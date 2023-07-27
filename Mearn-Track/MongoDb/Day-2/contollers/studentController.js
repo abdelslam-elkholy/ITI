8094092f7da2ab017dd6d128923b598f70cf5a13
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-27 03:14:00 +0300

    update

---
+++ Mearn-Track/MongoDb/Day-2/contollers/studentController.js
@@ -0,0 +1,15 @@
+const Student = require("./../models/studentModel");
+
+exports.createStudent = async (req, res) => {
+  try {
+    const newStudent = await Student.create(req.body);
+    res.status(201).json({
+      message: "success",
+      data: {
+        student: newStudent,
+      },
+    });
+  } catch (error) {
+    console.log(err);
+  }
+};
---
+++ Mearn-Track/MongoDb/Day-2/routs/studentRout.js
@@ -0,0 +1,3 @@
+const express = require("express");
+const studentController = require("./../contollers/studentController");
+const router = express.router();
