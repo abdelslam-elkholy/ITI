1ca6a52f662a6b17297fac1f8b62406986f77c27
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-08-02 13:12:42 +0300

    update

--- Mearn-Track/NodeJs/Day-2/controllers/todoController.js
+++ Mearn-Track/NodeJs/Day-2/controllers/todoController.js
@@ -10,7 +10,7 @@ exports.createTodo = async (req, res, next) => {
     const todo = req.body;
     todo.id = data.length ? data[data.length - 1].id + 1 : 1;
 
-    res.status(200).json({
+    res.status(201).json({
       message: "created",
       data: {
         todo,
@@ -51,9 +51,56 @@ exports.getTodo = (req, res, next) => {
     res.status(200).json({
       message: "success",
       data: {
-        data,
+        todo,
       },
     });
+    return;
+  }
+
+  res.status(404).json({
+    message: "Not Found",
+    err,
+  });
+};
+
+exports.updateTodo = (req, res, next) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const todoIndex = data.findIndex((el) => el.id == id);
+
+  if (todoIndex != -1) {
+    data[todoIndex].title = title;
+    writeToDb(data);
+    res.status(200).json({
+      message: "success",
+      data: {
+        todo: data[todoIndex],
+      },
+    });
+    return;
+  }
+
+  res.status(404).json({
+    message: "Not Found",
+    err,
+  });
+};
+
+exports.deleteTodo = (req, res, next) => {
+  const { id } = req.params;
+
+  const todoIndex = data.findIndex((el) => el.id == id);
+
+  if (todoIndex != -1) {
+    writeToDb(data);
+    res.status(200).json({
+      message: "success",
+      data: {
+        todo: data[todoIndex],
+      },
+    });
+    data.splice(todoIndex, 1);
     return;
   }
 
