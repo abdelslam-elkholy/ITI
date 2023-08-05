2ed0a55db1744e70fb6016a86a132e6ce4a1133e
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-08-05 06:41:11 +0300

    Final Edit

--- Mearn-Track/NodeJs/Day-3/app.js
+++ Mearn-Track/NodeJs/Day-3/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const todoRoute = require("./routes/todoRoute");
 const userRoute = require("./routes/userRoute");
+const errorHandler = require("./controllers/errorHandler");
 app.use(express.json());
 
 app.use("/api/todo", todoRoute);
 app.use("/api/user", userRoute);
+app.use(errorHandler);
 module.exports = app;
