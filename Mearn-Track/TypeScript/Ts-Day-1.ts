812169726a04966ab5b4baae1bd7f9d6d35d32c5
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-08-06 19:35:16 +0300

    update

--- Mearn-Track/TypeScript/Ts-Day-1.ts
+++ Mearn-Track/TypeScript/Ts-Day-1.ts
@@ -2,6 +2,14 @@ const num: number = 25;
 const str: string = "John";
 const bol: boolean = true;
 
+let uni: number | string;
+
+uni = 1;
+console.log(uni);
+
+uni = "str";
+console.log(uni);
+
 function addNumbers(num1: number, num2: number): number {
   return num1 + num2;
 }
