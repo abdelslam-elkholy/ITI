be811c534e54e0bff3a22fb6eeeda06160445355
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-08-07 04:07:47 +0300

    update

--- Mearn-Track/TypeScript/Ts-Day-1.ts
+++ Mearn-Track/TypeScript/Ts-Day-1.ts
@@ -74,3 +74,38 @@ const numClass = new NumberGen(10, 20);
 const strClass = new StringGen("a", "b");
 console.log(numClass);
 console.log(strClass);
+
+class point2D {
+  constructor(protected x: number, protected y: number) {}
+
+  calcLength(point: point2D): number {
+    const dX = this.x - point.x;
+    const dY = this.y - point.y;
+    const D = Math.sqrt(dX * dX + dY * dY);
+    return D;
+  }
+}
+
+class point3D extends point2D {
+  constructor(public x: number, public y: number, public z: number) {
+    super(x, y);
+  }
+
+  calcLength(point: point3D): number {
+    const dX = this.x - point.x;
+    const dY = this.y - point.y;
+    const dZ = this.z - point.z;
+    const D = Math.sqrt(dX * dX + dY * dY + dZ * dZ);
+    return D;
+  }
+}
+
+const poin2d1 = new point2D(5, 9);
+const poin2d2 = new point2D(3, 7);
+
+console.log(poin2d1.calcLength(poin2d2));
+
+const poin3d1 = new point3D(5, 9, 11);
+const poin3d2 = new point3D(3, 7, 9);
+
+console.log(poin3d1.calcLength(poin3d2));
